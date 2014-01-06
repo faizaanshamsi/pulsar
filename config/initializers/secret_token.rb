cd27ec81924d52edb476066c463373de90bb3699
faizaanshamsi <faizaanshamsi@gmail.com>
2014-01-06 13:52:20 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pulsar::Application.config.secret_key_base = 'db558d6bfcfa593ebe88d8f0a22d1671d2c4b6df8c45a1c7c6661a6f9a18fde6fea48bde96d9c00a4651d6cb1fb58156d401d0193f1e8abe418ca7cbceb70754'
