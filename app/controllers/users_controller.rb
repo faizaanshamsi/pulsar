248cdabe169d1fbc9452afe1d78650740d71957d
Eben <eben.lowe@gmail.com>
2014-01-07 15:50:17 -0500

    Resolved uniqueness of display name (all nil) bug.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def create
-    @user = User.new(user_params)
+    @user = User.find_or_create_by(user_params)
     if @user.save
       flash[:notice] = "Welcome to Pulsar."
       redirect_to new_learning_path
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   def user_params
     params['user'] = {}
     params['user']['name'] = env['omniauth.auth']['info']['nickname']
+    params['user']['display_name'] = env['omniauth.auth']['info']['nickname']
     params['user']['github_token'] = env['omniauth.auth']['credentials']['token']
     params['user']['github_uuid'] = env['omniauth.auth']['uid']
     params.require(:user).permit(:github_uuid, :name, :display_name, :github_token)
