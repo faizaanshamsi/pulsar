9f33af02bc08a356875ef425a7f2554177cecba0
Eben <eben.lowe@gmail.com>
2014-01-13 12:14:12 -0500

    Moved Github authentication into service object. Working now, but must test unhappy path.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def create
-    @user = User.find_or_create_by(user_params)
+    @user = GithubAuthenticator.authenticate(env['omniauth.auth'])
     if @user.save
       flash[:notice] = t('.success')
       redirect_to root_path
@@ -10,13 +10,4 @@ class UsersController < ApplicationController
       redirect_to root_path
     end
   end
-
-  private
-
-  def user_params
-    params = GithubAuthenticator.authenticate(env['omniauth.auth'], params)
-    binding.pry
-    params.require(:user).permit(:github_uid, :name, :display_name)
-  end
-
 end
--- app/models/github_authenticator.rb
+++ app/models/github_authenticator.rb
@@ -1,14 +1,11 @@
 class GithubAuthenticator
 
-  def initialize
-  end
-
-  def self.authenticate(env_hash, params)
-    @params = Hash.new
-    # @params['user'] = {}
-    @params['user']['name'] = env_hash['info']['nickname']
-    @params['user']['display_name'] = env_hash['info']['nickname']
-    @params['user']['github_uid'] = env_hash['uid']
-    @params
+  def self.authenticate(env_hash)
+    params = {}
+    params['user'] = {}
+    params['user']['name'] = env_hash['info']['nickname']
+    params['user']['display_name'] = env_hash['info']['nickname']
+    params['user']['github_uid'] = env_hash['uid']
+    User.find_or_create_by(github_uid: params['user']['github_uid'])
   end
 end
