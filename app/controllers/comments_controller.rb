eae0e29078f75572bea00bc030169c9602078bdd
Eben <eben.lowe@gmail.com>
2014-01-14 10:57:35 -0500

    Whitelisted user_id for comment params.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,7 +24,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body, :learning_id)
+    params.require(:comment).permit(:body, :learning_id, :user_id)
   end
 
 end
