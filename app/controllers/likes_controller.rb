117d8e698183a982071e62ece76f3335d35cb443
faizaanshamsi <faizaanshamsi@gmail.com>
2014-01-20 11:49:14 -0500

    create like failure message added

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,9 @@ class LikesController < ApplicationController
 
   def create
     @like = Like.new(learning_id: params[:learning_id], user_id: current_user.id)
-    @like.save
+    unless @like.save
+      flash[:error] = "Error! Like could not be saved for some reason. Try again or whatever."
+    end
     redirect_to learning_path(Learning.find(params[:learning_id]))
   end
 
