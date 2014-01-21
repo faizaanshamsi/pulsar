2cf9943467d60aa07a9ef6a091963ef206b157a6
Eben <eben.lowe@gmail.com>
2014-01-21 12:13:26 -0500

    Added Gravatar sample URL to test Login Helper to resolve failing tests.

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -7,7 +7,8 @@ module LoginHelper
       info: {
         nickname: user.display_name,
         email: nil,
-        name: user.name
+        name: user.name,
+        image: 'http://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?'
         }
       }
 
