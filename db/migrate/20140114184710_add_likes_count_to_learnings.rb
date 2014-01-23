f802aba12315cdd54b58a91b7f468639cad54508
Kevin <kvnkwon@gmail.com>
2014-01-22 20:32:10 -0500

    complete styling and default likes to 0

--- db/migrate/20140114184710_add_likes_count_to_learnings.rb
+++ db/migrate/20140114184710_add_likes_count_to_learnings.rb
@@ -1,5 +1,5 @@
 class AddLikesCountToLearnings < ActiveRecord::Migration
   def change
-    add_column :learnings, :likes_count, :integer
+    add_column :learnings, :likes_count, :integer, default: 0
   end
 end
